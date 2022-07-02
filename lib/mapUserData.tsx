ffd9581d2b82e1bb4ea5fbbe3b17d0409f841ab4
abdulahi roble <abdulahi_2610@hotmail.com>
2022-07-02 21:21:17 +0200

    Init firebase
    remove
    
    Delete .env
    
    Delete data-hoop-3d29d9058a6f.json

---
+++ lib/mapUserData.tsx
@@ -0,0 +1,12 @@
+import {User} from "firebase/auth"
+
+export const mapUserData = (user: any) => {
+    const {uid, email, xa, displayName, photoUrl} = user
+    return {
+        id: uid,
+        email,
+        token: xa,
+        name: displayName,
+        profilePic: photoUrl
+    }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type {NextPage} from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import {useUser} from '../lib/userUser'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const {user, logout} = useUser()
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
